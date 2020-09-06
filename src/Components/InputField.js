c202a4dfe6698991af6fa1015ecac92f2daea9b6
FedericoTartarini <federicotartarini@gmail.com>
2020-09-06 14:47:36 +0800

    * fixed error in InputField.js

--- src/Components/InputField.js
+++ src/Components/InputField.js
@@ -1,20 +1,20 @@
 import React from "react";
 
-function InputField({ handleChange, value, label }) {
+function InputField({ handleChange, data, id, label }) {
   return (
     <div className="w-1/3 px-3 my-1">
       <label
         className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-        htmlFor={label.split(" ")[0]}
+        htmlFor={id}
       >
         {label}
       </label>
       <input
         className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-        id={label.split(" ")[0]}
+        id={id}
         type="number"
-        name={label.split(" ")[0]}
-        placeholder={value}
+        name={id}
+        placeholder={data[id]}
         onChange={handleChange}
       />
     </div>
