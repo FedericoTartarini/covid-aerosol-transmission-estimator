f7a788bd32d12e2617f942f07fc52b3891fad069
FedericoTartarini <federicotartarini@gmail.com>
2020-09-20 22:52:18 +0800

    * lazy loading all components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 import React, { Suspense, useState } from "react";
-import Indoor from "./Components/Indoor";
-import Outdoor from "./Components/Outdoor";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Loader from "./Components/Loader";
-import NavigationBar from "./Components/NavigationBar";
 import { ThemeProvider } from "styled-components";
 import { lightTheme, darkTheme } from "./Functions/theme";
 import { GlobalStyles } from "./Functions/global";
@@ -12,6 +9,9 @@ const HomeView = React.lazy(() => import("./Components/HomeView"));
 const HelpPage = React.lazy(() => import("./Components/HelpPage"));
 const AboutView = React.lazy(() => import("./Components/AboutView"));
 const Footer = React.lazy(() => import("./Components/Footer"));
+const NavigationBar = React.lazy(() => import("./Components/NavigationBar"));
+const Indoor = React.lazy(() => import("./Components/Indoor"));
+const Outdoor = React.lazy(() => import("./Components/Outdoor"));
 
 function App() {
   const [theme, setTheme] = useState(getTheme);
@@ -34,12 +34,12 @@ function App() {
   }
 
   return (
-    <ThemeProvider theme={theme === "light" ? lightTheme : darkTheme}>
-      <GlobalStyles />
-      <Router basename="/">
-        <div className="relative pb-10 min-h-screen">
-          <NavigationBar themeToggler={themeToggler} theme={theme} />
-          <Suspense fallback={<Loader />}>
+    <Suspense fallback={<Loader />}>
+      <ThemeProvider theme={theme === "light" ? lightTheme : darkTheme}>
+        <GlobalStyles />
+        <Router basename="/">
+          <div className="relative pb-10 min-h-screen">
+            <NavigationBar themeToggler={themeToggler} theme={theme} />
             <Switch>
               <Route exact path="/">
                 <HomeView />
@@ -56,10 +56,10 @@ function App() {
               <Route path="/help/:id" component={HelpPage} />
             </Switch>
             <Footer />
-          </Suspense>
-        </div>
-      </Router>
-    </ThemeProvider>
+          </div>
+        </Router>
+      </ThemeProvider>
+    </Suspense>
   );
 }
 
