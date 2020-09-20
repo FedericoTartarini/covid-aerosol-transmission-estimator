33bcda2f0f617a5ce7c32a9e98bd999bb26e6e66
FedericoTartarini <federicotartarini@gmail.com>
2020-09-20 10:24:46 +0800

    * changed authors Footer.js

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -11,33 +11,43 @@ function Footer() {
             <p className="text-xs ml-3">v 0.0.3</p>
           </div>
         </span>
-        <p className="text-sm sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">
-          © 2020
-          <a
-            href="https://www.linkedin.com/in/federico-tartarini"
-            className="ml-1"
-            rel="noopener noreferrer"
-            target="_blank"
-          >
-            Federico Tartarini,
-          </a>
-          <a
-            href="https://www.linkedin.com/in/asitk/"
-            className="ml-1"
-            rel="noopener noreferrer"
-            target="_blank"
-          >
-            Asit Mishra,
-          </a>
-          <a
-            href="https://twitter.com/jljcolorado"
-            className="ml-1"
-            rel="noopener noreferrer"
-            target="_blank"
-          >
-            Jose L. Jimenez
-          </a>
-        </p>
+        <div className="flex flex-col">
+          <p className="flex-1 text-sm sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">
+            © 2020
+            <a
+              href="https://www.linkedin.com/in/federico-tartarini"
+              className="ml-1"
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              Federico Tartarini,
+            </a>
+            <a
+              href="https://www.linkedin.com/in/asitk/"
+              className="ml-1"
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              Asit Mishra,
+            </a>
+          </p>
+          <span className="flex-1 text-sm sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">
+            The current tool is based on
+            <a
+              className="mx-1"
+              href="https://docs.google.com/spreadsheets/d/16K1OQkLD4BjgBdO8ePj6ytf-RpPMlJ6aXFg3PrIQBbQ/edit#gid=519189277"
+            >
+              the estimator
+            </a>
+            created by
+            <a
+              className="mx-1"
+              href="https://www.colorado.edu/chemistry/jose-luis-jimenez"
+            >
+              Prof. J L Jimenez
+            </a>
+          </span>
+        </div>
         <span className="inline-flex  sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
           <a
             className="mx-3"
